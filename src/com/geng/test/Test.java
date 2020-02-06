0dcd013fb8180f4f4a92f1d1ea2b4918afa4f28c
gengjihui_maya@163.com <gengjihui_maya@163.com>
2020-02-07 05:19:16 +0800

    session 的三种查询方式：                       selectOne:返回Object，适用于返回结果是一个变量，或一行数据时                       selectList：返回List,适用于需要查询全部结果                       selectMap:需要在返回的查询结果中，再次进行过滤，通过某列的值，获取这列数据

--- src/com/geng/test/Test.java
+++ src/com/geng/test/Test.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 测试类
@@ -18,10 +19,16 @@ public class Test {
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = factory.openSession();
+        //查询所有数据
         List<Flower> list = session.selectList("a.b.selectAll");
         for (Flower flower : list) {
             System.out.println(flower.toString());
         }
+        //查询数据行数
+        int i = session.selectOne("a.b.selectOne");
+        System.out.println(i);
+        Map<Object, Object> map = session.selectMap("a.b.selectMap", "name");
+        System.out.println(map);
         session.close();
     }
 }
